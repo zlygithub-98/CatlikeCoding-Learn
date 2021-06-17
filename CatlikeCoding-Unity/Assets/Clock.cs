192f7d0f24c709d90c2b83f235d066c8fd548873
ZLY <1738242284@qq.com>
2021-06-17 16:24:18 +0800

    add clock:DateTime.Now.TimeOfDay.TotalHours ：当前小时换算成小数

--- CatlikeCoding-Unity/Assets/Clock.cs
+++ CatlikeCoding-Unity/Assets/Clock.cs
@@ -3,28 +3,20 @@ using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
-    [SerializeField] Transform hoursPivot = default, minutesPivot, secondsPivot;
+    [SerializeField] Transform hoursPivot, minutesPivot, secondsPivot;
     const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
 
-    void Awake()
+    private void Start()
     {
-        DateTime time = DateTime.Now;
-        hoursPivot.localRotation =
-            Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
-        minutesPivot.localRotation =
-            Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
-        secondsPivot.localRotation =
-            Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+        Debug.Log("Hour默认(整数的)数据：" + DateTime.Now.Hour);
+        Debug.Log("TotalHours小数数据：" + DateTime.Now.TimeOfDay.TotalHours);
     }
 
     void Update()
     {
         TimeSpan time = DateTime.Now.TimeOfDay;
-        hoursPivot.localRotation =
-            Quaternion.Euler(0f, 0f, (float) (hoursToDegrees * time.TotalHours));
-        minutesPivot.localRotation =
-            Quaternion.Euler(0f, 0f, (float) (minutesToDegrees * time.TotalMinutes));
-        secondsPivot.localRotation =
-            Quaternion.Euler(0f, 0f, (float) (secondsToDegrees * time.TotalSeconds));
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (hoursToDegrees * time.TotalHours));
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (minutesToDegrees * time.TotalMinutes));
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (secondsToDegrees * time.TotalSeconds));
     }
 }
