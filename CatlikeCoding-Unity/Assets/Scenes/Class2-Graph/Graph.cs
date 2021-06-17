7351da4280efd4c0f58d5544f7af3eb72016527f
ZLY <1738242284@qq.com>
2021-06-17 17:19:38 +0800

    add 部分graph

---
+++ CatlikeCoding-Unity/Assets/Scenes/Class2-Graph/Graph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] private Transform pointPrefab;
+    void Awake () {
+        Vector3 position=default;
+        var scale = Vector3.one / 5f;
+        for (int i = 0; i < 10; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+}
