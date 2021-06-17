0ba25afc9f638d93203df7ebbe53018b407c10bd
ZLY <1738242284@qq.com>
2021-06-17 16:08:45 +0800

    add 第一节clock

---
+++ CatlikeCoding-Unity/Assets/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] Transform hoursPivot = default, minutesPivot, secondsPivot;
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    void Awake()
+    {
+        DateTime time = DateTime.Now;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+    }
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation =
+            Quaternion.Euler(0f, 0f, (float) (hoursToDegrees * time.TotalHours));
+        minutesPivot.localRotation =
+            Quaternion.Euler(0f, 0f, (float) (minutesToDegrees * time.TotalMinutes));
+        secondsPivot.localRotation =
+            Quaternion.Euler(0f, 0f, (float) (secondsToDegrees * time.TotalSeconds));
+    }
+}
