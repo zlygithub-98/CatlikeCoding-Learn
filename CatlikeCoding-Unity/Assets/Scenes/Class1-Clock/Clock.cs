60892e20fef23bba03688ebc433803876bcf45fd
ZLY <1738242284@qq.com>
2021-06-23 16:50:28 +0800

    update 新的进度

---
+++ CatlikeCoding-Unity/Assets/Scenes/Class1-Clock/Clock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] Transform hoursPivot, minutesPivot, secondsPivot;
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+
+    private void Start()
+    {
+        Debug.Log("Hour默认(整数的)数据：" + DateTime.Now.Hour);
+        Debug.Log("TotalHours小数数据：" + DateTime.Now.TimeOfDay.TotalHours);
+    }
+
+    void Update()
+    {
+        TimeSpan time = DateTime.Now.TimeOfDay;
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (hoursToDegrees * time.TotalHours));
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (minutesToDegrees * time.TotalMinutes));
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, (float) (secondsToDegrees * time.TotalSeconds));
+    }
+}
--- CatlikeCoding-Unity/Assets/Scenes/Class2-Graph/Graph.cs
+++ CatlikeCoding-Unity/Assets/Scenes/Class2-Graph/Graph.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     [SerializeField] private Transform pointPrefab;
-    void Awake () {
-        Vector3 position=default;
-        var scale = Vector3.one / 5f;
-        for (int i = 0; i < 10; i++) {
+
+    //必须在10-100之间
+    [SerializeField, Range(10, 100)] private int resolution = 10;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+        for (int i = 0; i < resolution; i++)
+        {
             Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
-            position.y = position.x;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;
             point.localPosition = position;
             point.localScale = scale;
+            point.SetParent(transform,false);
         }
     }
 }
